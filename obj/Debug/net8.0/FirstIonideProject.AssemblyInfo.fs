4bdc988e81c841362855739d70c0eaa7ee043943
yashelter <yashelter@yandex.ru>
2024-04-28 23:16:10 +0300

    hotfix

--- obj/Debug/net8.0/FirstIonideProject.AssemblyInfo.fs
+++ obj/Debug/net8.0/FirstIonideProject.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("FirstIonideProject")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbdb0ec937f35f395fa9d5c99ca373a8c4310f96")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42ed2eb6d6aa2ee4a07c83a756b39fdd0f52d883")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("FirstIonideProject")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("FirstIonideProject")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
